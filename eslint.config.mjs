4ed61294d6297bdb73a54570e13601d88545531e
hari mohan <harimohan@haris-MacBook-Air-2.local>
2026-02-18 17:48:20 +0530

    hello

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import js from "@eslint/js";
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: [".next", "node_modules", "*.config.*"] },
-  js.configs.recommended,
-];
